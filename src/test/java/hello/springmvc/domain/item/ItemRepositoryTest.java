7f32a6ee66f8c695351e286a2de42cce3208ff30
ksungsu <zxzxzlxlzlxl@naver.com>
2022-12-10 01:36:25 +0900

    상품 도메인 개발 테스트

--- src/test/java/hello/springmvc/domain/item/ItemRepositoryTest.java
+++ src/test/java/hello/springmvc/domain/item/ItemRepositoryTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ItemRepositoryTest {
@@ -31,18 +33,30 @@ public class ItemRepositoryTest {
     @Test
     void findAll(){
         //given
-        Item item = new Item();
+
+        Item itemA = new Item("itemA", 10000, 10);
+        Item itemB = new Item("itemB", 20000, 20);
+
+        itemRepository.save(itemA);
+        itemRepository.save(itemB);
 
         //when
+        List<Item> findAllItem = itemRepository.findAll();
+
         //then
+        assertThat(findAllItem.size()).isEqualTo(2);
+        assertThat(findAllItem).contains(itemA, itemB);
     }
 
     @Test
     void updateItem(){
         //given
-        Item item = new Item();
+//        Item itemA = new Item("itemA", 10000, 10);
+//        Item itemAId = itemRepository.findById(itemA.getId());
+//
+//        //when
+//        itemRepository.update(itemAId, );
 
-        //when
         //then
     }
 }
