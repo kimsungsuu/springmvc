7583f5a3efa661c5a67028c2ec3d00d5a4875a83
ksungsu <zxzxzlxlzlxl@naver.com>
2022-12-15 03:23:01 +0900

    상품 controller

--- src/main/java/hello/springmvc/web/basic/BasicItemController.java
+++ src/main/java/hello/springmvc/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -77,10 +78,14 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String saveV5(Item item) {
-        itemRepository.save(item);
+    public String saveV5(Item item, RedirectAttributes redirectAttributes) {
 
-        return "redirect:/basic/items";
+        Item savedItem = itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
     }
     /**
      * 테스트용 데이터 추가
